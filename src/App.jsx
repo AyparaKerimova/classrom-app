d186d37a39ddbd5a04651b26eed9c36c16b3dba3
AyparaKerimova <eelify1@gmail.com>
2024-12-27 11:04:55 +0400

    login register

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import TeacherRoute from "./routes/TeacherRoute"
 import StudentRoute from "./routes/StudentRoute"
+import Login from "./pages/common/Login"
+import Register from "./pages/common/Register"
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
       <Routes> 
         <Route path="/teachers/*" element={<TeacherRoute/>}/>
         <Route path="/students/*" element={<StudentRoute/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </>
   )

21c8c9763e6ea2aec54e33d55e5f0f847b5b9645
AyparaKerimova <eelify1@gmail.com>
2024-12-27 19:29:22 +0400

    finished register

--- src/validations/user.register.validation.js
+++ src/validations/user.register.validation.js
@@ -1,15 +1,12 @@
 import * as Yup from 'yup';
 
-export const userSchema = Yup.object().shape({
-    fullName: Yup.string().required("full name is required"),
-    username: Yup.string().required("username is required"),
-    email: Yup.string().email().required("email is required"),
-    password: Yup.string().matches(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-        "Minimum eight characters, at least one letter and one number:"
-    ).required("password is required"),
-    profileImage:Yup.mixed().required("profile image is required"),
-    major:Yup.string().required("major is required"),
-    bio:Yup.string(),
-    socialLinks:Yup.mixed()
-});
+export const userSchema = Yup.object({
+    fullName: Yup.string().required("Full name is required"),
+    userName: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email format").required("Email is required"),
+    password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+    profileImage: Yup.mixed().required("Profile image is required"),
+    role: Yup.string().required("Role is required"),
+    major: Yup.string(),
+    bio: Yup.string()
+  });

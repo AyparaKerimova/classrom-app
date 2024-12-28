bca864ca93d561ad0cb42611fb4e05d79f10f4a1
AyparaKerimova <eelify1@gmail.com>
2024-12-28 10:08:34 +0400

    update fix urls

--- src/features/api.js
+++ src/features/api.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_API_URL } from '../constants/api.js';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }), 
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }), 
   endpoints: (builder) => ({
     login: builder.query({
       query: () => ({

8d37de60b0b26fc26cd407065943bc236d250552
AyparaKerimova <eelify1@gmail.com>
2025-01-06 02:15:19 +0400

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@tinymce/tinymce-react', 'tinymce'],
+    include: ['@tinymce/tinymce-react', 'tinymce', 'react-quill'], 
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-quill/dist/quill.snow.css'], 
+    },
   },
   assetsInclude: ['**/*.JPG', '**/*.jpg'], 
-})
+});

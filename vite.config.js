9804e447feb7dd744a189c99a3f080db1c7bc784
AyparaKerimova <126576758+AyparaKerimova@users.noreply.github.com>
2025-01-06 01:57:36 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@tinymce/tinymce-react', 'tinymce'],
+    include: ['@tinymce/tinymce-react', 'tinymce', 'react-quill'], // react-quill eklendi
   },
-  assetsInclude: ['**/*.JPG', '**/*.jpg'], 
-})
+  build: {
+    rollupOptions: {
+      external: ['react-quill/dist/quill.snow.css'], // CSS dosyasını harici olarak işaretle
+    },
+  },
+  assetsInclude: ['**/*.JPG', '**/*.jpg'], // JPG uzantılı dosyalar dahil
+});
